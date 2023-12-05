cd6273f80f4a379d2b38ec04e7335f627e281eae
Riley DuBord <thelifeofdu@gmail.com>
2023-12-05 12:07:31 -0600

    function created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,9 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 myCollection = [];
 
-
+function addToCollection(collection, title, artist, yearPublished) {
+  
+}
 
 
 
